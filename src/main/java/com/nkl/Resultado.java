c82ec312ed9731231f532b4e832540b24cea87b0
Nikol <143750294+nar-ran@users.noreply.github.com>
2024-03-05 17:17:42 -0500

    Validaciones

--- src/main/java/com/nkl/Resultado.java
+++ src/main/java/com/nkl/Resultado.java
@@ -1,5 +1,9 @@
 package com.nkl;
 
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
 public class Resultado {
     private String textoTxt;
     private double resultado;
@@ -13,7 +17,16 @@ public class Resultado {
 
         if(validacion==0){
             System.out.println("txt = " + textoTxt);
-            System.out.println("El resultado de la operación es: "+resultado);
+
+            ScriptEngineManager m = new ScriptEngineManager();
+            ScriptEngine engine = m.getEngineByName("JavaScript");
+
+            try {
+                Object r = engine.eval(textoTxt);
+                System.out.println("El resultado de la operación "+textoTxt+" es: "+resultado);
+            } catch (Exception e) {
+                System.err.println("Ha ocurrido un error."+ e);
+            }
         }
 //            System.err.println("Error al hacer la operación.");
     }
--- src/main/java/com/nkl/Validacion.java
+++ src/main/java/com/nkl/Validacion.java
@@ -7,26 +7,32 @@ public class Validacion {
     private int validacion = 0;
 
     public int validarTexto(String text) {
+        Pattern pattern = Pattern.compile("[a-zA-Z]");
+        Matcher matcher = pattern.matcher(text);
+
         /*
-        * Primero debo validar los tipos de datos, que no sean caracteres como lo es una letra
-        * Segundo, validar que solo contenga los signos de operaciones básicas.
         * Tercero, validar que no tenga operadores diferentes a los especificados.
         * Cuarto, validar que el orden de los signos sea correcto.
         */
 
-        Pattern pattern = Pattern.compile("[a-zA-Z]");
-        Matcher matcher = pattern.matcher(text);
+        Matcher mp = Pattern.compile("[\\d*-/+]").matcher(text);
+        if (!mp.find()){
+            System.err.println("El txt no tiene una estructura valida");
+        }
 
-        if(matcher.find()){ // Validación de tipo de caracteres del txt
+
+        if(matcher.find()){
             validacion = 1;
             System.err.println("El archivo txt contiene letras.");
-        }else if(!Pattern.compile("[-+*/]").matcher(text).find()){ // Validacion de signos de operación
-            validacion = 2;
-            System.err.println("La operación no es valida para calcular.");
-        }else if(Pattern.compile("").matcher(text).find()){
-            validacion = 3;
-            System.err.println("EL archivo contiene caracteres invalidos");
         }
+//        else if(!Pattern.compile("[-+*/]").matcher(text).find()){ // Validacion de signos de operación
+//            validacion = 2;
+//            System.err.println("La operación no es valida para calcular.");
+//        }else if(!Pattern.compile("\\d").matcher(text).find()){
+//            validacion = 3;
+//            System.out.println("No contiene numeros el programa. ");
+////            System.err.println("EL archivo contiene caracteres invalidos");
+//        }
 
         return validacion;
     }
